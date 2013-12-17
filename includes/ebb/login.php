96a399bc536e53eb6f0c91030bcb94c6a370cc23
eman1986 <maggot05@gmail.com>
2013-12-17 14:00:46 -0500

    more work on login object

--- includes/ebb/login.php
+++ includes/ebb/login.php
@@ -38,10 +38,9 @@ class login {
      *@return bool
    */
     private function validateUser() {
-
         try {
             //check against the database to see if the username  match.
-            $query = $this->db->prepare('SELECT count(id) from ebb_users WHERE Username=:username LIMIT 1');
+            $query = $this->db->prepare('SELECT COUNT(id) FROM ebb_users WHERE Username=:username LIMIT 1');
             $query->execute(array(":username" => $this->usr));
             $validateUser = $query->fetchColumn();
 
@@ -59,14 +58,19 @@ class login {
      * @return bool
     */
     private function validatePwd() {
+        try {
+            //check against the database to see if the password match.
+            $query = $this->db->prepare('SELECT Password FROM ebb_users WHERE Username=:username LIMIT 1');
+            $query->execute(array(":username" => $this->usr));
+            $hashedPwd = $query->fetchColumn();
 
-        //check against the database to see if the password match.
-        $query = $this->db->prepare('SELECT Password from ebb_users WHERE Username=:username LIMIT 1');
-        $query->execute(array(":username" => $this->usr));
-        $hashedPwd = $query->fetchColumn();
-
-        //see if password is value.
-        return password_verify($this->pwd, $hashedPwd) ? false : true;
+            //see if password is value.
+            return password_verify($this->pwd, $hashedPwd) ? false : true;
+        }
+        catch (PDOException $e) {
+            echo $e->getMessage();
+            return false;
+        }
     }
 
     /**
@@ -75,13 +79,18 @@ class login {
      * @return boolean
     */
     private function ValidateLoginKey($key) {
+        try {
+            $query = $this->db->prepare('SELECT COUNT(login_key) FROM ebb_login_session WHERE username=:username');
+            $query->execute(array(":username" => $key));
+            $validateKey = $query->fetchColumn();
 
-        $query = $this->db->prepare('SELECT count(login_key) from ebb_login_session WHERE username=:username');
-        $query->execute(array(":username" => $key));
-        $validateKey = $query->fetchColumn();
-
-        #setup bool. value to see if user is active or not.
-        return $validateKey == 0 ? false : true;
+            #setup bool. value to see if user is active or not.
+            return $validateKey == 0 ? false : true;
+        }
+        catch (PDOException $e) {
+            echo $e->getMessage();
+            return false;
+        }
     }
 
     /**
@@ -193,50 +202,9 @@ class login {
 
     /**
      * Performs login process, creating any sessions or cookies needed for the system.
-     * @param boolean $remember keep user login info in tact?
-     * @version 07/28/12
-     * @access public
-     */
-//    public function logOn($remember){
-//
-//        global $db;
-//
-//        #setup variables.
-//        $loginKey = sha1(makeRandomPassword());
-//        $lastActive = time() + 300;
-//
-//        #see if user wants to remain logged on.
-//        if($remember) {
-//            //setup session length.
-//            $expireTime = time() + (2592000);
-//
-//            //create cookie.
-//            setcookie("ebbuser", $this->user, $expireTime, '/', $_SERVER['SERVER_NAME'], isSecure() ? 1 : 0, true);
-//
-//            #remove user's IP from who's online list.
-//            $db->delete("ebb_online", "ip = :ip", array(":ip" => $ipAddr));
-//
-//            $_SESSION['ebb_login_key'] = $loginKey;
-//
-//            //generate session-based validation.
-//            $this->regenerateSession(true);
-//        } else {
-//            //create a session.
-//            $_SESSION['ebb_user'] = $this->user;
-//            $_SESSION['ebb_login_key'] = $loginKey;
-//
-//            //generate session-based validation.
-//            $this->regenerateSession(true);
-//        }
-//    }
-
-    /**
-     * Performs login process, creating any sessions or cookies needed for the system.
      * @param bool $remember
     */
-    public function logOn($remember=FALSE){
-
-        global $ipAddr, $db;
+    public function logOn($remember=FALSE) {
 
         //see if user wants to remain logged on.
         if ($remember) {
@@ -252,9 +220,9 @@ class login {
             //create cookie.
             setcookie("ebbuser", $this->user, $expireTime, '/', $_SERVER['SERVER_NAME'], isSecure() ? 1 : 0, TRUE);
 
-            #remove user's IP from who's online list.
-            $db->SQL = "delete from ebb_online where ip='$ipAddr'";
-            $db->query();
+            //remove user's IP from who's online list.
+            $query = $this->db->prepare('DELETE FROM ebb_online WHERE ip=:ip');
+            $query->execute(array(":ip" => detectProxy()));
 
             //generate session-based validation.
             $this->regenerateSession(TRUE);
@@ -265,8 +233,6 @@ class login {
      * Performs logout process, removing any sessions or cookies created from the system.
     */
     public function logOut() {
-
-        global $db;
 
         #setup session length.
         $expireAcp = time()-3600;
@@ -323,8 +289,7 @@ class login {
                 $this->regenerateSession($destroy);
             }
         } catch(Exception $e) {
-            $error = new notifySys($e, TRUE, TRUE, __FILE__, __LINE__);
-            $error->genericError();
+            echo $e->getMessage();
         }
     }
 
@@ -350,164 +315,109 @@ class login {
      * @return bool
     */
     public function isActive(){
-        global $db;
-
         //@TODO MAY NOT NEED THIS ANYMORE!
         //check against the database to see if the username and password match.
-        $db->SQL = "SELECT active FROM ebb_users WHERE Username='".$this->user."' LIMIT 1";
-        $validateStatus = $db->fetchResults();
+//        $db->SQL = "SELECT active FROM ebb_users WHERE Username='".$this->user."' LIMIT 1";
+//        $validateStatus = $db->fetchResults();
 
         #setup bool. value to see if user is active or not.
-        if ($validateStatus['active'] == 0) {
-            return FALSE;
-        } else {
-            return TRUE;
-        }
+//        if ($validateStatus['active'] == 0) {
+//            return FALSE;
+//        } else {
+//            return TRUE;
+//        }
     }
 
     /**
-	*deactivateUser
-	*
-	*disable user's active status.
-	*
-	*@modified 10/26/09
-	*
-	*@access public
+	 * disable user's active status.
 	*/
 	public function deactivateUser(){
-
         //@TODO MAY NOT NEED THIS ANYMORE!
-
-		global $db;
-		
-		$db->SQL = "UPDATE ebb_users SET active='0' WHERE Username='".$this->user."' LIMIT 1";
-		$db->query();
+		//$db->SQL = "UPDATE ebb_users SET active='0' WHERE Username='".$this->user."' LIMIT 1";
+		//$db->query();
 	}
 
 	/**
-	*activateUser
-	*
-	*activates the user to allow them access the system.
-	*
-	*@modified 10/26/09
-	*
-	*@access public
+	 * activates the user to allow them access the system.
 	*/
 	public function activateUser(){
 
         //@TODO MAY NOT NEED THIS ANYMORE!
-
-		global $db;
-
-		$db->SQL = "UPDATE ebb_users SET active='1' WHERE Username='".$this->user."' LIMIT 1";
-		$db->query();
+		//$db->SQL = "UPDATE ebb_users SET active='1' WHERE Username='".$this->user."' LIMIT 1";
+		//$db->query();
 	}
 
     /**
-	*getFailedLoginCt
-	*
-	*See how many times the user failed to login correctly.
-	*
-	*@modified 8/10/09
-	*
-	*
-	*@access public
+	 * See how many times the user failed to login correctly.
 	*/
 	public function getFailedLoginCt(){
-
         //@TODO MAY NOT NEED THIS ANYMORE!
-
-	    global $db;
-
-	    #get the count from the user table..
-        $db->SQL = "SELECT failed_attempts FROM ebb_users WHERE Username='".$this->user."' LIMIT 1";
-		$getFailedLoginCt = $db->fetchResults();
-
-		return($getFailedLoginCt);
+        #get the count from the user table..
+        //$db->SQL = "SELECT failed_attempts FROM ebb_users WHERE Username='".$this->user."' LIMIT 1";
+		//$getFailedLoginCt = $db->fetchResults();
+		//return($getFailedLoginCt);
 	}
 
     /**
-	*setFailedLogin
-	*
-	*increment fail count for defined user.
-	*
-	*@modified 7/24/11
-	*
-	*
-	*@access public
+	 * increment fail count for defined user.
 	*/
 	public function setFailedLogin(){
-
-	    global $db;
-
         //@TODO MAY NOT NEED THIS ANYMORE!
 
-	    #get new count.
-		$newCount = $this->getFailedLoginCt();
-		$incrementFailedCt = $newCount['failed_attempts'] + 1;
+	    //#get new count.
+		//$newCount = $this->getFailedLoginCt();
+		//$incrementFailedCt = $newCount['failed_attempts'] + 1;
 
 	    #get the count from the user table.
-        $db->SQL = "UPDATE ebb_users SET failed_attempts='$incrementFailedCt' WHERE Username='".$this->user."' LIMIT 1";
-		$db->query();
+        //$db->SQL = "UPDATE ebb_users SET failed_attempts='$incrementFailedCt' WHERE Username='".$this->user."' LIMIT 1";
+		//$db->query();
 	}
 
     /**
-	*clearFailedLogin
-	*
-	*Clears the failed count to 0.
-	*
-	*@modified 8/10/09
-	*
-	*
-	*@access public
+	 * Clears the failed count to 0.
 	*/
 	public function clearFailedLogin(){
-
-	    global $db;
-
         //@TODO MAY NOT NEED THIS ANYMORE!
 
-	    #clear count.
-        $db->SQL = "UPDATE ebb_users SET failed_attempts='0' WHERE Username='".$this->user."' LIMIT 1";
-		$db->query();
+        #clear count.
+        //$db->SQL = "UPDATE ebb_users SET failed_attempts='0' WHERE Username='".$this->user."' LIMIT 1";
+		//$db->query();
 	}
 
     /**
-	*checkBan
-	*
-	*Checks to see if the user is banned or suspended.
+	 * Checks to see if the user is banned or suspended.
 	*/
 	public function checkBan() {
-        global $db, $lang, $suspend_length, $suspend_date, $groupProfile;
+        global $suspend_length, $suspend_date, $groupProfile;
 
         //@TODO MAY NOT NEED THIS ANYMORE OR SHOULD BE REFACTORED!
 
         //see if user is marked as banned.
-        if ($groupProfile == 6) {
-            $error = new notifySys($lang['banned'], TRUE);
-        }
+//        if ($groupProfile == 6) {
+//            $error = new notifySys($lang['banned'], TRUE);
+//        }
 
         //see if user is suspended.
-        if ($suspend_length > 0) {
-            #see if user is still suspended.
-            $math = 3600 * $suspend_length;
-            $suspend_time = $suspend_date + $math;
-            $today = time() - $math;
-            if($suspend_time > $today){
-                $error = new notifySys($lang['suspended'], TRUE);
-                $error->displayError();
-            }
-        }
+//        if ($suspend_length > 0) {
+//            #see if user is still suspended.
+//            $math = 3600 * $suspend_length;
+//            $suspend_time = $suspend_date + $math;
+//            $today = time() - $math;
+//            if($suspend_time > $today){
+//                $error = new notifySys($lang['suspended'], TRUE);
+//                $error->displayError();
+//            }
+//        }
 
         #see if the IP of the user is banned.
-        $uip = detectProxy();
-        $db->SQL = "SELECT ban_item FROM ebb_banlist WHERE ban_type='IP' AND ban_item LIKE '%$uip%'";
-        $banChk = $db->affectedRows();
-
-        #output an error msg.
-        if ($banChk == 1) {
-            $error = new notifySys($lang['banned'], TRUE);
-            $error->displayError();
-        }
+//        $uip = detectProxy();
+//        $db->SQL = "SELECT ban_item FROM ebb_banlist WHERE ban_type='IP' AND ban_item LIKE '%$uip%'";
+//        $banChk = $db->affectedRows();
+//
+//        #output an error msg.
+//        if ($banChk == 1) {
+//            $error = new notifySys($lang['banned'], TRUE);
+//            $error->displayError();
+//        }
     }
 }
